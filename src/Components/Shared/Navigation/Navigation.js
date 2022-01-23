79f311e7b0eeb937105de775c8ad3216528b2aba
Pranta07 <palpranta@gmail.com>
2022-01-24 01:26:27 +0600

    disclosure button styles added

--- src/Components/Shared/Navigation/Navigation.js
+++ src/Components/Shared/Navigation/Navigation.js
@@ -145,7 +145,7 @@ const Navigation = () => {
                                     </div>
                                     <div className="-mr-2 flex md:hidden">
                                         {/* Mobile menu button */}
-                                        <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                        <Disclosure.Button className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                             <span className="sr-only">
                                                 Open main menu
                                             </span>
