757711f30e7a1a3a04f346957d4ecfd482cf8f8f
Anik Dhar <aniksutradhar11@gmail.com>
2022-01-24 13:28:00 +0600

    just checked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from team project 2..</h1>
+      <h1>Hello from team project..</h1>
+      <h2>I am anik, just checked it.</h2>
     </div>
   );
 }
