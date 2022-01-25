d3db84396a7e83e46bab8c3b87f925b1c8a3c87f
Pranta07 <palpranta@gmail.com>
2022-01-25 15:32:30 +0600

    height issue fixed

--- src/Components/HeroSection/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection/HeroSection.js
@@ -1,25 +1,6 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
 
-const IMAGES = [
-    {
-        imageUrl: "https://loremflickr.com/600/300/paris",
-        placeHolder: "Paris",
-    },
-    {
-        imageUrl: "https://loremflickr.com/600/300/dog",
-        placeHolder: "Dog",
-    },
-    {
-        imageUrl: "https://loremflickr.com/600/300/cat",
-        placeHolder: "Cat",
-    },
-    {
-        imageUrl: "https://loremflickr.com/600/300/forest",
-        placeHolder: "Forest",
-    },
-];
-
 const HeroSection = () => {
     return (
         <div className="">
