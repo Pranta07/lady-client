1d3d320f388917c2ce612bb275f484776c51d15e
Pranta07 <palpranta@gmail.com>
2022-01-25 15:47:39 +0600

    controll button position changed

--- src/Components/HeroSection/Carousel/Carousel.js
+++ src/Components/HeroSection/Carousel/Carousel.js
@@ -31,7 +31,7 @@ const Carousel = () => {
                 </div>
             </div>
             <button
-                className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                className="carousel-control-prev w-6 absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                 type="button"
                 data-bs-target="#carouselExampleControls"
                 data-bs-slide="prev"
@@ -43,13 +43,13 @@ const Carousel = () => {
                 <span className="visually-hidden">Previous</span>
             </button>
             <button
-                className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                className="carousel-control-next w-6 absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                 type="button"
                 data-bs-target="#carouselExampleControls"
                 data-bs-slide="next"
             >
                 <span
-                    className="carousel-control-next-icon inline-block bg-no-repeat bg-black py-10 m-0"
+                    className="carousel-control-next-icon inline-block bg-no-repeat bg-black py-10"
                     aria-hidden="true"
                 ></span>
                 <span className="visually-hidden">Next</span>
