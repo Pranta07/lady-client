1762da3c3e308c6aa059dd7b685746c2ee0a08b9
Pranta07 <palpranta@gmail.com>
2022-01-25 20:22:59 +0600

    Trending Header Done

---
+++ src/Components/Trending/Trending.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Trending = () => {
+    return (
+        <div>
+            {/* Heading Part */}
+            <div className="my-3">
+                <p className="uppercase py-4 tracking-widest">
+                    The new arrivals
+                </p>
+                <h1 className="uppercase text-4xl font-bold tracking-widest">
+                    Trending
+                </h1>
+                <p className="w-1/5 h-3 bg-yellow-400 my-4 mx-auto "></p>
+                <p className="tracking-widest">
+                    DEFINE YOUR WARDROBE WITH ACCESSORIES
+                </p>
+            </div>
+
+            {/* Products Section */}
+            <div></div>
+        </div>
+    );
+};
+
+export default Trending;
