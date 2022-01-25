238e72ce2a4a6550d463ed276875d3ce9f9d9f7c
Pranta07 <palpranta@gmail.com>
2022-01-26 02:47:28 +0600

    hover-effect applied

--- src/Components/DressCollection/DressCollection.js
+++ src/Components/DressCollection/DressCollection.js
@@ -191,7 +191,7 @@ const DressCollection = () => {
                     <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         {products.map((product, index) => (
                             <div key={index} className="group relative">
-                                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:scale-110 duration-300 lg:h-80 lg:aspect-none">
                                     <img
                                         src={product.imgUrl}
                                         alt=""
