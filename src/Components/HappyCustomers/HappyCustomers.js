e9de7063310411fae4dbdb026d953e55f1935424
Pranta07 <palpranta@gmail.com>
2022-01-27 01:49:30 +0600

    sliding issue solved

--- src/Components/HappyCustomers/HappyCustomers.js
+++ src/Components/HappyCustomers/HappyCustomers.js
@@ -31,7 +31,7 @@ const HappyCustomers = () => {
 
             {/* Reviews */}
             <div
-                id="carouselExampleControls"
+                id="carouselReview"
                 className="carousel slide relative pb-12"
                 data-bs-ride="carousel"
             >
@@ -60,7 +60,7 @@ const HappyCustomers = () => {
                 <button
                     class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                     type="button"
-                    data-bs-target="#carouselExampleControls"
+                    data-bs-target="#carouselReview"
                     data-bs-slide="prev"
                 >
                     <span
@@ -72,7 +72,7 @@ const HappyCustomers = () => {
                 <button
                     class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                     type="button"
-                    data-bs-target="#carouselExampleControls"
+                    data-bs-target="#carouselReview"
                     data-bs-slide="next"
                 >
                     <span
