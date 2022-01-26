59e0e77ba39bf335804eb71efcf60278d7e18725
Pranta07 <palpranta@gmail.com>
2022-01-27 02:07:45 +0600

    top products added

--- src/Components/DressGallery/DressGallery.js
+++ src/Components/DressGallery/DressGallery.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Featured from "../Featured/Featured";
 import NewIn from "../NewIn/NewIn";
+import Top from "../Top/Top";
 
 const DressGallery = () => {
     return (
         <div className="mx-4 pb-10 flex">
             <NewIn></NewIn>
             <Featured></Featured>
+            <Top></Top>
         </div>
     );
 };
