ff1f51343930a5ccc2d48c8d4dd5aca75269a77f
Pranta07 <palpranta@gmail.com>
2022-01-26 21:16:07 +0600

    Customer Reviews Added

--- src/Components/HappyCustomers/HappyCustomers.js
+++ src/Components/HappyCustomers/HappyCustomers.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+const reviews = [
+    {
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi temporibus accusantiumagnam consectetur optio porro ex numquamrecusandae cum corrupti.",
+        name: "Pranta Pal",
+    },
+    {
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi temporibus accusantiumagnam consectetur optio porro ex numquamrecusandae cum corrupti.",
+        name: "Ema Watsom",
+    },
+    {
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi temporibus accusantiumagnam consectetur optio porro ex numquamrecusandae cum corrupti.",
+        name: "David Josh",
+    },
+];
+
 const HappyCustomers = () => {
     return (
         <div className="bg-black text-white">
-            <div className="my-3">
+            {/* Heading */}
+            <div className="my-3 p-4">
                 <p className="uppercase py-4 tracking-widest">
                     WHAT DOES THE FOX SAY?
                 </p>
@@ -11,6 +27,61 @@ const HappyCustomers = () => {
                     HAPPY CUSTOMERS
                 </h1>
                 <p className="w-1/5 h-3 bg-yellow-400 my-5 mx-auto "></p>
+            </div>
+
+            {/* Reviews */}
+            <div
+                id="carouselExampleControls"
+                className="carousel slide relative pb-8"
+                data-bs-ride="carousel"
+            >
+                <div className="carousel-inner relative mx-auto w-2/3 overflow-hidden bg-yellow-400 text-black rounded-md">
+                    {reviews.map((review, index) => (
+                        <div
+                            key={index}
+                            className={
+                                index === 0
+                                    ? "carousel-item active relative float-left w-full"
+                                    : "carousel-item relative float-left w-full"
+                            }
+                        >
+                            <div className="w-2/3 h-80 mx-auto flex flex-col justify-center">
+                                <p className="text-xl tracking-wide">
+                                    {review.desc}
+                                </p>
+                                <h1 className="text-2xl tracking-wide font-bold m-4">
+                                    {review.name}
+                                </h1>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <button
+                    className="carousel-control-prev w-6 absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-24"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="prev"
+                >
+                    <span
+                        className="carousel-control-prev-icon inline-block bg-no-repeat bg-gray-900 py-10"
+                        aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+
+                <button
+                    className="carousel-control-next w-6 absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-24"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="next"
+                >
+                    <span
+                        className="carousel-control-next-icon inline-block bg-no-repeat bg-gray-900 py-10"
+                        aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
     );
