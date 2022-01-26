941096ebb7b03f05a0159f39106a083304f7532f
Pranta07 <palpranta@gmail.com>
2022-01-26 20:06:45 +0600

    heading added

---
+++ src/Components/HappyCustomers/HappyCustomers.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HappyCustomers = () => {
+    return (
+        <div className="bg-black text-white">
+            <div className="my-3">
+                <p className="uppercase py-4 tracking-widest">
+                    WHAT DOES THE FOX SAY?
+                </p>
+                <h1 className="uppercase text-4xl font-bold tracking-widest">
+                    HAPPY CUSTOMERS
+                </h1>
+                <p className="w-1/5 h-3 bg-yellow-400 my-5 mx-auto "></p>
+            </div>
+        </div>
+    );
+};
+
+export default HappyCustomers;
