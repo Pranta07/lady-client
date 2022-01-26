1e8b93ef59f0098d2d14f362a90877d873f60a79
Pranta07 <palpranta@gmail.com>
2022-01-27 03:18:13 +0600

    all device responsive

--- src/Components/DressGallery/DressGallery.js
+++ src/Components/DressGallery/DressGallery.js
@@ -5,7 +5,7 @@ import Top from "../Top/Top";
 
 const DressGallery = () => {
     return (
-        <div className="container mx-auto md:px-6 pb-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0">
+        <div className="container mx-auto sm:px-3 md:px-6 pb-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0">
             <NewIn></NewIn>
             <Featured></Featured>
             <Top></Top>
