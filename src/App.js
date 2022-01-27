fdc2a895c5b1dd9974f9da33329a07f642e30e1e
Pranta07 <palpranta@gmail.com>
2022-01-27 17:47:34 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import "./App.css";
-import DressCollection from "./Components/DressCollection/DressCollection";
-import DressGallery from "./Components/DressGallery/DressGallery";
-import HappyCustomers from "./Components/HappyCustomers/HappyCustomers";
-import HeroSection from "./Components/HeroSection/HeroSection/HeroSection";
+import Home from "./Components/Home/Home";
 // import NotFound from "./Components/NotFound/NotFound";
-import Navigation from "./Components/Shared/Navigation/Navigation";
-import SubscribeSection from "./Components/SubscribeSection/SubscribeSection";
-import Trending from "./Components/Trending/Trending";
 
 function App() {
     return (
         <div className="App bg-gray-200">
-            <Navigation></Navigation>
-            <HeroSection></HeroSection>
-            <Trending></Trending>
-            <DressCollection></DressCollection>
-            <HappyCustomers></HappyCustomers>
-            <DressGallery></DressGallery>
-            <SubscribeSection></SubscribeSection>
+            <Home></Home>
             {/* <NotFound></NotFound> */}
         </div>
     );
