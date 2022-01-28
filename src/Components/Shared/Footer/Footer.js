a74244c6e59f497d8c41fdb56b3ae6d08a43a8d2
Pranta07 <palpranta@gmail.com>
2022-01-28 18:05:16 +0600

    routing issue solved

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,79 +1,92 @@
-import React from 'react';
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div className='pt-16 bg-black text-white'>
-            <div className='grid lg:grid-cols-4 text-white sm:grid-cols-1 md:grid-cols-2 sm:grid-cols-1 justify-items-center gap-4'>
+        <div className="pt-16 bg-black text-white">
+            <div className="grid lg:grid-cols-4 text-white sm:grid-cols-1 md:grid-cols-2 sm:grid-cols-1 justify-items-center gap-4">
                 <div>
-                    <h2 className='hover:underline-offset-8 title font-bold hover:text-slate-400'>QUICK-LINKS</h2>
-                    <div className='flex flex-col mt-8 link text-justify font-normal'>
+                    <h2 className="hover:underline-offset-8 title font-bold hover:text-slate-400">
+                        QUICK-LINKS
+                    </h2>
+                    <div className="flex flex-col mt-8 link text-justify font-normal">
                         <a href="#myAccount">MY ACCOUNT</a>
                         <a href="#contactUs"> CONTACT US</a>
                         <a href="#storeLocation"> STORE LOCATION</a>
                         <a href="#sizeGuide">SIZE GUIDE</a>
-                        <a className='box' href="#shippingInfo">  SHIPPING INFO</a>
+                        <a className="box" href="#shippingInfo">
+                            {" "}
+                            SHIPPING INFO
+                        </a>
                     </div>
                 </div>
-                <div className='text-justify'>
-                    <h2 className='hover:underline-offset-8 title font-bold hover:text-slate-400'>RECENT BLOGS</h2>
+                <div className="text-justify">
+                    <h2 className="hover:underline-offset-8 title font-bold hover:text-slate-400">
+                        RECENT BLOGS
+                    </h2>
 
-                    <div className='mt-8 flex flex-col blogs-item'>
+                    <div className="mt-8 flex flex-col blogs-item">
                         <div>
-                            <p>
-                                TOP 10 TRENDING DRESS THIS SUM
-                            </p>
+                            <p>TOP 10 TRENDING DRESS THIS SUM</p>
                         </div>
                         <div>
-                            <p>
-                                THE AWESOME CUTE STACK
-                            </p>
+                            <p>THE AWESOME CUTE STACK</p>
                         </div>
                         <div>
-                            <p>
-                                NEWS ABOUT OPEN HOUR OF STORE
-                            </p>
+                            <p>NEWS ABOUT OPEN HOUR OF STORE</p>
                         </div>
                     </div>
                 </div>
-                <div className='text-justify'>
-                    <h2 className='hover:underline-offset-8 title font-bold hover:text-slate-400'>TAGS</h2>
-                    <div className='grid mt-8 box font-normal'>
-                        <div className='flex items-center'>
+                <div className="text-justify">
+                    <h2 className="hover:underline-offset-8 title font-bold hover:text-slate-400">
+                        TAGS
+                    </h2>
+                    <div className="grid mt-8 box font-normal">
+                        <div className="flex items-center">
                             <span> DRESS</span>
-                            <span className='ml-6'> LIPS</span>
+                            <span className="ml-6"> LIPS</span>
                         </div>
                         <span> DOTTED</span>
                         <span> ACCESSORIES</span>
-                        <div className='flex items-center'>
+                        <div className="flex items-center">
                             <span> DRESS</span>
-                            <span className='ml-6'> CROSS</span>
+                            <span className="ml-6"> CROSS</span>
                         </div>
                         <span> ABSTRACT</span>
                         <span> BABYDOLL</span>
                         <span> SALEOFF</span>
                         <span> FLORAL</span>
-
                     </div>
                 </div>
-                <div className='text-justify'>
-                    <h2 className='hover:underline-offset-8 title font-bold hover:text-slate-400'>FIND US ON</h2>
-                    <div className='mt-8 font'>
+                <div className="text-justify">
+                    <h2 className="hover:underline-offset-8 title font-bold hover:text-slate-400">
+                        FIND US ON
+                    </h2>
+                    <div className="mt-8 font">
                         <i class="fab fa-facebook-f"></i>
                         <i class="fab fa-twitter "></i>
                         <i class="fab fa-instagram "></i>
                         <i class="fab fa-google-plus-g "></i>
                     </div>
-                    <div className=' mx-2 mt-6'>
-                        <h4 className='font-bold'> @AKES</h4>
-                        <p className='text-sm hover:text-slate-400'> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <h4 className='font-bold'> @FTPS</h4>
-                        <p className='text-sm hover:text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div className=" mx-2 mt-6">
+                        <h4 className="font-bold"> @AKES</h4>
+                        <p className="text-sm hover:text-slate-400">
+                            {" "}
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit.
+                        </p>
+                        <h4 className="font-bold"> @FTPS</h4>
+                        <p className="text-sm hover:text-slate-400">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit.
+                        </p>
                     </div>
                 </div>
             </div>
-            <hr className='bg-green-200 w-5/6 mx-auto mt-8 hover:bg-cyan-600' />
-            <p className='p-4 font-normal text-slate-100 hover:text-slate-400'>COPYRIGHT 2022 BY Hero-Team-Mate . ALL RIGHTS RESERVED</p>
+            <hr className="bg-green-200 w-5/6 mx-auto mt-8 hover:bg-cyan-600" />
+            <p className="p-4 font-normal text-slate-100 hover:text-slate-400">
+                COPYRIGHT 2022 BY Hero-Team-Mate . ALL RIGHTS RESERVED
+            </p>
         </div>
     );
 };
