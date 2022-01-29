73d12d57552c1e2413a8aa2335728a09e9744c19
Pranta07 <palpranta@gmail.com>
2022-01-29 23:35:16 +0600

    page reload issue solved

--- src/Components/HappyCustomers/HappyCustomers.js
+++ src/Components/HappyCustomers/HappyCustomers.js
@@ -58,28 +58,28 @@ const HappyCustomers = () => {
                 </div>
 
                 <button
-                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                     type="button"
                     data-bs-target="#carouselReview"
                     data-bs-slide="prev"
                 >
                     <span
-                        class="carousel-control-prev-icon inline-block bg-no-repeat"
+                        className="carousel-control-prev-icon inline-block bg-no-repeat"
                         aria-hidden="true"
                     ></span>
-                    <span class="visually-hidden">Previous</span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
                 <button
-                    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                     type="button"
                     data-bs-target="#carouselReview"
                     data-bs-slide="next"
                 >
                     <span
-                        class="carousel-control-next-icon inline-block bg-no-repeat"
+                        className="carousel-control-next-icon inline-block bg-no-repeat"
                         aria-hidden="true"
                     ></span>
-                    <span class="visually-hidden">Next</span>
+                    <span className="visually-hidden">Next</span>
                 </button>
             </div>
         </div>
