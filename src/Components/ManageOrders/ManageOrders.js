05f5293350a83c74e52509af710d5d7fca642af0
Pranta07 <palpranta@gmail.com>
2022-02-12 22:12:24 +0600

    loading spinner added

--- src/Components/ManageOrders/ManageOrders.js
+++ src/Components/ManageOrders/ManageOrders.js
@@ -4,11 +4,14 @@ import React, { useEffect, useState } from "react";
 const ManageOrders = () => {
     const [orders, setOrders] = useState([]);
     const [isDelete, setIsDelete] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         fetch(`http://localhost:5000/allOrders`)
             .then((res) => res.json())
-            .then((data) => setOrders(data));
+            .then((data) => setOrders(data))
+            .finally(() => setLoading(false));
     }, [isDelete]);
 
     const handleDelete = (id) => {
@@ -37,80 +40,91 @@ const ManageOrders = () => {
                     </span>
                 </p>
             </div>
-            <div className="bg-gray-50 p-6 md:p-8 xl:p-14">
-                <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {orders.map((order) => (
-                        <div
-                            key={order._id}
-                            className="relative p-4 border border-yellow-300 rounded-md shadow-md"
-                        >
-                            <div
-                                onClick={() => handleDelete(order._id)}
-                                className="absolute top-2 right-2 hover:text-red-600 cursor-pointer"
-                            >
-                                <TrashIcon className="h-6 w-6"></TrashIcon>
-                            </div>
-                            <div className="text-left">
-                                <p>
-                                    <span className="font-semibold">
-                                        Customer Email:{" "}
-                                    </span>
-                                    {order.cus_email}
-                                </p>
-                                <p>
-                                    <span className="font-semibold">
-                                        TrxId:{" "}
-                                    </span>
-                                    {order.tran_id}
-                                </p>
-                                <p>
-                                    <span className="font-semibold">
-                                        Total Cost:{" "}
-                                    </span>
-                                    {order.currency}
-                                    {order.total_amount}
-                                </p>
-                                <p>
-                                    <span className="font-semibold">
-                                        Payment Status:{" "}
-                                    </span>
-                                    {order.payment_status ? "PAID" : "PENDING"}
-                                </p>
-                            </div>
-                            <h2 className="text-xl my-2 font-semibold tracking-widest">
-                                Ordered Items
-                            </h2>
-                            {order.ordered_products?.map((product) => (
-                                <div
-                                    key={product._id}
-                                    className="border p-2 flex items-center text-left"
-                                >
-                                    <div className="w-1/6">
-                                        <img
-                                            src={product.imgUrl}
-                                            alt=""
-                                            className="w-full h-20"
-                                        />
-                                    </div>
-
-                                    <div className="m-2">
-                                        <h1 className="text-xl font-semibold tracking-widest">
-                                            {product.name}
-                                        </h1>
-                                        <p className="text-sm font-semibold tracking-widest">
-                                            Price: {order.currency}
-                                            {product.price}
-                                        </p>
-                                        <p className="text-sm font-semibold tracking-widest">
-                                            Quantity: {product.quantity}
-                                        </p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ))}
+            {loading ? (
+                <div className="m-10">
+                    <svg
+                        className="animate-spin h-5 w-5 bg-yellow-400 mx-auto"
+                        viewBox="0 0 24 24"
+                    ></svg>
                 </div>
-            </div>
+            ) : (
+                <div className="bg-gray-50 p-6 md:p-8 xl:p-14">
+                    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {orders.map((order) => (
+                            <div
+                                key={order._id}
+                                className="relative p-4 border border-yellow-300 rounded-md shadow-md"
+                            >
+                                <div
+                                    onClick={() => handleDelete(order._id)}
+                                    className="absolute top-2 right-2 hover:text-red-600 cursor-pointer"
+                                >
+                                    <TrashIcon className="h-6 w-6"></TrashIcon>
+                                </div>
+                                <div className="text-left">
+                                    <p>
+                                        <span className="font-semibold">
+                                            Customer Email:{" "}
+                                        </span>
+                                        {order.cus_email}
+                                    </p>
+                                    <p>
+                                        <span className="font-semibold">
+                                            TrxId:{" "}
+                                        </span>
+                                        {order.tran_id}
+                                    </p>
+                                    <p>
+                                        <span className="font-semibold">
+                                            Total Cost:{" "}
+                                        </span>
+                                        {order.currency}
+                                        {order.total_amount}
+                                    </p>
+                                    <p>
+                                        <span className="font-semibold">
+                                            Payment Status:{" "}
+                                        </span>
+                                        {order.payment_status
+                                            ? "PAID"
+                                            : "PENDING"}
+                                    </p>
+                                </div>
+                                <h2 className="text-xl my-2 font-semibold tracking-widest">
+                                    Ordered Items
+                                </h2>
+                                {order.ordered_products?.map((product) => (
+                                    <div
+                                        key={product._id}
+                                        className="border p-2 flex items-center text-left"
+                                    >
+                                        <div className="w-1/6">
+                                            <img
+                                                src={product.imgUrl}
+                                                alt=""
+                                                className="w-full h-20"
+                                            />
+                                        </div>
+
+                                        <div className="m-2">
+                                            <h1 className="text-xl font-semibold tracking-widest">
+                                                {product.name}
+                                            </h1>
+                                            <p className="text-sm font-semibold tracking-widest">
+                                                Price: {order.currency}
+                                                {product.price}
+                                            </p>
+                                            <p className="text-sm font-semibold tracking-widest">
+                                                Quantity: {product.quantity}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </>
     );
 };
--- src/Components/MyOrders/MyOrders.js
+++ src/Components/MyOrders/MyOrders.js
@@ -5,12 +5,15 @@ import useFirebase from "../../hooks/useFirebase";
 
 const MyOrders = () => {
     const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { user } = useFirebase();
 
     useEffect(() => {
+        setLoading(true);
         fetch(`http://localhost:5000/orders?email=${user?.email}`)
             .then((res) => res.json())
-            .then((data) => setOrders(data));
+            .then((data) => setOrders(data))
+            .finally(() => setLoading(false));
     }, [user?.email]);
     // console.log(orders);
 
@@ -24,68 +27,79 @@ const MyOrders = () => {
                     </span>
                 </p>
             </div>
-            <div className="bg-gray-50 p-6 md:p-8 xl:p-14">
-                <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {orders.map((order) => (
-                        <div
-                            key={order._id}
-                            className="p-4 border border-yellow-300 rounded-md shadow-md"
-                        >
-                            <div className="text-left">
-                                <p>
-                                    <span className="font-semibold">
-                                        TrxId:{" "}
-                                    </span>
-                                    {order.tran_id}
-                                </p>
-                                <p>
-                                    <span className="font-semibold">
-                                        Total Cost:{" "}
-                                    </span>
-                                    {order.currency}
-                                    {order.total_amount}
-                                </p>
-                                <p>
-                                    <span className="font-semibold">
-                                        Payment Status:{" "}
-                                    </span>
-                                    {order.payment_status ? "PAID" : "PENDING"}
-                                </p>
-                            </div>
-                            <h2 className="text-xl my-2 font-semibold tracking-widest">
-                                Ordered Items
-                            </h2>
-                            {order.ordered_products?.map((product) => (
-                                <div
-                                    key={product._id}
-                                    className="border p-2 flex items-center text-left"
-                                >
-                                    <div className="w-1/6">
-                                        <img
-                                            src={product.imgUrl}
-                                            alt=""
-                                            className="w-full h-20"
-                                        />
-                                    </div>
-
-                                    <div className="m-2">
-                                        <h1 className="text-xl font-semibold tracking-widest">
-                                            {product.name}
-                                        </h1>
-                                        <p className="text-sm font-semibold tracking-widest">
-                                            Price: {order.currency}
-                                            {product.price}
-                                        </p>
-                                        <p className="text-sm font-semibold tracking-widest">
-                                            Quantity: {product.quantity}
-                                        </p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ))}
+            {loading ? (
+                <div className="m-10">
+                    <svg
+                        className="animate-spin h-5 w-5 bg-yellow-400 mx-auto"
+                        viewBox="0 0 24 24"
+                    ></svg>
                 </div>
-            </div>
+            ) : (
+                <div className="bg-gray-50 p-6 md:p-8 xl:p-14">
+                    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {orders.map((order) => (
+                            <div
+                                key={order._id}
+                                className="p-4 border border-yellow-300 rounded-md shadow-md"
+                            >
+                                <div className="text-left">
+                                    <p>
+                                        <span className="font-semibold">
+                                            TrxId:{" "}
+                                        </span>
+                                        {order.tran_id}
+                                    </p>
+                                    <p>
+                                        <span className="font-semibold">
+                                            Total Cost:{" "}
+                                        </span>
+                                        {order.currency}
+                                        {order.total_amount}
+                                    </p>
+                                    <p>
+                                        <span className="font-semibold">
+                                            Payment Status:{" "}
+                                        </span>
+                                        {order.payment_status
+                                            ? "PAID"
+                                            : "PENDING"}
+                                    </p>
+                                </div>
+                                <h2 className="text-xl my-2 font-semibold tracking-widest">
+                                    Ordered Items
+                                </h2>
+                                {order.ordered_products?.map((product) => (
+                                    <div
+                                        key={product._id}
+                                        className="border p-2 flex items-center text-left"
+                                    >
+                                        <div className="w-1/6">
+                                            <img
+                                                src={product.imgUrl}
+                                                alt=""
+                                                className="w-full h-20"
+                                            />
+                                        </div>
+
+                                        <div className="m-2">
+                                            <h1 className="text-xl font-semibold tracking-widest">
+                                                {product.name}
+                                            </h1>
+                                            <p className="text-sm font-semibold tracking-widest">
+                                                Price: {order.currency}
+                                                {product.price}
+                                            </p>
+                                            <p className="text-sm font-semibold tracking-widest">
+                                                Quantity: {product.quantity}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </>
     );
 };
