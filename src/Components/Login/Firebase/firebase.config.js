1ecb3e0e2a072111a55d7afc97a75b0a3cbc4538
Anik Dhar <aniksutradhar11@gmail.com>
2022-02-18 18:17:50 +0600

    intial-commit

--- src/Components/Login/Firebase/firebase.config.js
+++ src/Components/Login/Firebase/firebase.config.js
@@ -1,14 +1,5 @@
 // const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCyt8bRSwQE5NVzTt8klaBnvR-mtRG3mYQ",
-    authDomain: "team-project-a2f6d.firebaseapp.com",
-    projectId: "team-project-a2f6d",
-    storageBucket: "team-project-a2f6d.appspot.com",
-    messagingSenderId: "438878072877",
-    appId: "1:438878072877:web:19f978bd1ece50d41f08a4",
-    measurementId: "G-DM1SWRN9FY"
-  };
-
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 export default firebaseConfig;
 
